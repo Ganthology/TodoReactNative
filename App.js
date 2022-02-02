bbec835bf11cf8017f430df521ffd911c2e446d2
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-02-02 21:09:23 +0800

    Add react-native-toast-message package and setup

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigator from './navigation/RootStackNavigator';
+import Toast from 'react-native-toast-message';
 
 const App = () => {
   return (
     <NavigationContainer>
       <RootStackNavigator />
+      <Toast />
     </NavigationContainer>
   );
 };
