70a3981e75e42969e85cc5a6a7decbf21bcf19f1
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-02-03 20:01:11 +0800

    Add splash screen to app using react-native-splash-screen library

--- App.js
+++ App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigator from './navigation/RootStackNavigator';
 import Toast from 'react-native-toast-message';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <NavigationContainer>
       <RootStackNavigator />
